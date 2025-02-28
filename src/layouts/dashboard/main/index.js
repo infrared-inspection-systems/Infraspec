393e3440ae995c636879346c869695c4fb36083d
DaneIIS <dstephenson@iisthermal.com>
2025-02-28 07:21:32 -0800

    Change License

--- src/layouts/dashboard/main/index.js
+++ src/layouts/dashboard/main/index.js
@@ -1,17 +1,3 @@
-/**
-=========================================================
-* Material Dashboard 3 PRO React - v2.3.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-pro-react
-* Copyright 2024 Infraspec (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
 import { useRef, useEffect, useState, useMemo } from "react";
 
 // porp-types is a library for typechecking of props
